7b1b1ed7049faaf4d1a46058f4f9d381d5c598ec
yashashk <yashaskumar888@gmail.com>
2023-08-24 12:44:08 +0530

    updated solution

--- Array/bubbleSort.js
+++ Array/bubbleSort.js
@@ -2,12 +2,18 @@
 
 const bubbleSort = (nums) => {
   for (let i = nums.length; i > 0; i--) {
+    // console.log(i);
+    let isSwapped = false;
     for (let j = 0; j < i - 1; j++) {
       if (nums[j] > nums[j + 1]) {
         [nums[j], nums[j + 1]] = [nums[j + 1], nums[j]];
+        isSwapped = true;
       }
+    }
+    if (!isSwapped) {
+      break;
     }
   }
   return nums;
 };
-console.log(bubbleSort([11, 23, 4, 5, 6, 77, 33, 4, 52, 24, 3]));
+console.log(bubbleSort([10, 1, 2, 3, 4, 5]));
